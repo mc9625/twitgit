648ba483acda358a6aebd60d3516a7eb98a34835
mc9625 <massimo@MacBookPro.lan>
2021-05-17 22:28:30 +0200

    primo invio

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM continuumio/miniconda3
+
+LABEL maintainer="shihao1007@gmail.com"
+
+RUN conda install -y -c conda-forge pandas nltk plotly dash
+
+RUN apt-get update
+
+RUN apt-get install -y sqlite3 libsqlite3-dev
+
+ADD ./ /app
+WORKDIR /app
+
+EXPOSE 80
+
+CMD python /app/app.py
---
+++ data_gathering/keys_secret.py
@@ -0,0 +1,5 @@
+# put your keys and secret here
+consumer_key = 'OsJbqM1l2AxN2TmBFJgQH2LWA'
+consumer_secret = 'zZXGpqrv7jxR9Ppskq8T90b01l20fgqDQdJvq6QqEzLRSh1CJ4'
+access_token = '342756962-8Va7urYrVuHGU8zQEUduvpQWKvGR96u2KDrA9wYQ'
+access_token_secret = '6c8v2mMo3SSU1E54dwmICyK7clJqJ6fHPM3NSTjdA9KED'
